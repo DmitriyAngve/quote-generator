f3a68c47d84ba0303ba263529f3a4fb84dd1bc42
DmitriyAngve <kirirch129@gmail.com>
2023-06-21 18:51:05 +0300

    #

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+      displayName: true,
+      // Enabled by default.
+      ssr: true,
+      // Enabled by default.
+      fileName: true,
+      // Empty by default.
+      topLevelImportPaths: [],
+      // Defaults to ["index"].
+      meaninglessFileNames: ["index"],
+      // Enabled by default.
+      cssProp: true,
+      // Empty by default.
+      namespace: "",
+      // Not supported yet.
+      minify: false,
+      // Not supported yet.
+      transpileTemplateLiterals: false,
+      // Not supported yet.
+      pure: false,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+// "topLevelImportPaths, meaninglessFileNames, cssProp, namespace" - позволяют настроить дополнительные функции и поведение styled-components.
+// В целом используется для в проекте для настройки и расширения функциональности фреймворка
