1caca49000d3d26150b9a30e44593038f2f4df38
DmitriyAngve <kirirch129@gmail.com>
2023-06-23 15:53:23 +0300

    #

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -34,3 +34,28 @@ export const BackgroundImage2 = styled(Image)`
   right: -120px;
   bottom: -10px;
 `;
+
+export const FooterCon = styled.div`
+  width: 100vw;
+  height: 50px;
+  text-align: center;
+  font-family: "Source Code Pro", monospace;
+  font-size: 15px;
+  position: absolute;
+  bottom: 0;
+  color: white;
+  z-index: 999999;
+`;
+
+export const FooterLink = styled(Link)`
+  color: white;
+`;
+
+export const RedSpan = styled.span`
+  font-size: 1.5em;
+  background: linear-gradient(to right, #ed0404, #630516);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  animation: gradient 2s ease infinite;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,20 @@ import Head from "next/head";
 import {
   BackgroundImage1,
   BackgroundImage2,
+  FooterCon,
   GradientBackground,
+  FooterLink,
+  RedSpan,
 } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
 // Assets
 import Clouds1 from "@/assets/cloud-and-thunder.png";
 import Clouds2 from "@/assets/cloudy-weather.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -23,7 +29,23 @@ export default function Home() {
       {/* Background */}
       <GradientBackground>
         <BackgroundImage1 src={Clouds1} height="300" alt="cloudybackground1" />
+
         <BackgroundImage2 src={Clouds2} height="300" alt="cloudybackground2" />
+
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Develope with <RedSpan>♥</RedSpan> by{" "}
+            <FooterLink
+              href="https://github.com/DmitriyAngve"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @DmitriyAngve{" "}
+            </FooterLink>
+          </>
+        </FooterCon>
       </GradientBackground>
     </>
   );
